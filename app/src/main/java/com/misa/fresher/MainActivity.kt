1665e69a20901cb39e53dccb5847c49ff6928787
NGUYEN THAI BAO <baopzzoo@gmail.com>
2022-02-25 16:58:55 +0700

    fix2 second commit

--- app/src/main/java/com/misa/fresher/MainActivity.kt
+++ app/src/main/java/com/misa/fresher/MainActivity.kt
@@ -12,22 +12,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var adpter = RcvAdapter()
+        configView()
         setData()
-        adpter?.list = data
+    }
+
+    fun configView() {
+        var adpter = RcvAdapter().also { it.list = data }
         rcvListProduct.adapter = adpter
     }
 
     fun setData() {
         data.addAll(
-            listOf(Product("BAC1", "tui xach 1", 1234.0, R.drawable.ic_shopping_bag),
+            listOf(
+                Product("BAC1", "tui xach 1", 1234.0, R.drawable.ic_shopping_bag),
                 Product("BAC2", "tui xach 2", 2312.0, R.drawable.ic_shopping_bag),
                 Product("BAC3", "tui xach 3", 1239.0, R.drawable.ic_shopping_bag),
                 Product("BAC4", "tui xach 4", 5667.0, R.drawable.ic_shopping_bag),
                 Product("BAC5", "tui xach 5", 9999.0, R.drawable.ic_shopping_bag),
                 Product("BAC6", "tui xach 6", 9999.0, R.drawable.ic_shopping_bag),
                 Product("BAC7", "tui xach 7", 9999.0, R.drawable.ic_shopping_bag),
-                Product("BAC8", "tui xach 8", 9999.0, R.drawable.ic_shopping_bag))
+                Product("BAC8", "tui xach 8", 9999.0, R.drawable.ic_shopping_bag)
+            )
         )
+        rcvListProduct.adapter?.notifyDataSetChanged()
     }
 }
