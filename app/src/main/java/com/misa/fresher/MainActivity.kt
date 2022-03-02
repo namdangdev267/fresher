efbf4de2e72e04d5679d585f47be9b647421b64e
NGUYEN THAI BAO <baopzzoo@gmail.com>
2022-03-02 12:03:18 +0700

    Update ship fragment and package fragment

--- app/src/main/java/com/misa/fresher/MainActivity.kt
+++ app/src/main/java/com/misa/fresher/MainActivity.kt
@@ -2,38 +2,20 @@ package com.misa.fresher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ui.Product
-import com.example.ui.RcvAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.misa.fresher.adapters.ProductsAdapter
+import com.misa.fresher.model.Product.Companion.createProductsList
+
 
 class MainActivity : AppCompatActivity() {
-    val data = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        configView()
-        setData()
-    }
-
-    fun configView() {
-        var adpter = RcvAdapter().also { it.list = data }
-        rcvListProduct.adapter = adpter
-    }
-
-    fun setData() {
-        data.addAll(
-            listOf(
-                Product("BAC1", "tui xach 1", 1234.0, R.drawable.ic_shopping_bag),
-                Product("BAC2", "tui xach 2", 2312.0, R.drawable.ic_shopping_bag),
-                Product("BAC3", "tui xach 3", 1239.0, R.drawable.ic_shopping_bag),
-                Product("BAC4", "tui xach 4", 5667.0, R.drawable.ic_shopping_bag),
-                Product("BAC5", "tui xach 5", 9999.0, R.drawable.ic_shopping_bag),
-                Product("BAC6", "tui xach 6", 9999.0, R.drawable.ic_shopping_bag),
-                Product("BAC7", "tui xach 7", 9999.0, R.drawable.ic_shopping_bag),
-                Product("BAC8", "tui xach 8", 9999.0, R.drawable.ic_shopping_bag)
-            )
-        )
-        rcvListProduct.adapter?.notifyDataSetChanged()
+        val rcv = findViewById<RecyclerView>(R.id.rcvListProduct)
+        val adapter = ProductsAdapter(createProductsList(20))
+        rcv.adapter=adapter
+        rcv.layoutManager=LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/misa/fresher/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ui
-
-data class Product(
-    val id: String,
-    val name: String,
-    val price: Double,
-    val img: Int
-)
