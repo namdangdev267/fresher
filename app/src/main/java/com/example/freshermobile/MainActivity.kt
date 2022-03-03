623c5f653d233304fbccbbf4245da3adc2fc1a90
longhoang99 <hoanggialong009@gmail.com>
2022-03-04 00:46:51 +0700

    add deliveryActivity with tabLayout+viewpager and fragment

--- app/src/main/java/com/example/freshermobile/MainActivity.kt
+++ app/src/main/java/com/example/freshermobile/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.example.freshermobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
+import android.widget.ImageButton
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.freshermobile.model.ProductModel
 
 class MainActivity : AppCompatActivity() {
-    private var listProduct: MutableList<ProductModel>?= null
+    private var listProduct = mutableListOf<ProductModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.hide()
+        supportActionBar?.hide()
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        listProduct = mutableListOf()
         for (i in 0 until 50) {
             val productModel = ProductModel()
             productModel.name = ("MaSP ".plus(i))
-            productModel.number = i
+            productModel.amount = i
             productModel.price = 0.0F
-            listProduct?.add(productModel)
+            listProduct.add(productModel)
         }
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
         recyclerView.itemAnimator = DefaultItemAnimator()
         recyclerView.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
-        recyclerView.adapter = ProductAdapter(listProduct!!)
+        recyclerView.adapter = ProductAdapter(listProduct)
+        val btnCart = findViewById<ImageButton>(R.id.button_cart)
+        btnCart.setOnClickListener {
+            startActivity(Intent(this,DeliveryActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/freshermobile/model/ProductModel.kt
+++ app/src/main/java/com/example/freshermobile/model/ProductModel.kt
@@ -1,10 +1,10 @@
-package com.example.freshermobile
+package com.example.freshermobile.model
 
 import android.widget.ImageView
 import android.widget.TextView
 
 class ProductModel {
     var name: String?= null
-    var number: Int?= null
+    var amount: Int?= null
     var price: Float?= null
 }
