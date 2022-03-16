5b7a35cde41fe15ecc3cf88e1071a54aa68ff898
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2022-03-16 17:48:47 +0700

    Base

---
+++ app/src/main/java/com/misa/fresher/Base/BaseAdapter.kt
@@ -0,0 +1,15 @@
+package com.misa.fresher.Base
+
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<T,V : BaseViewHolder<T>>: RecyclerView.Adapter<V>() {
+    abstract var adapterData : MutableList<T>
+
+    abstract var clickItemListener: (T) -> Unit
+
+    override fun getItemCount(): Int = adapterData.size
+
+    override fun onBindViewHolder(holder: V, position: Int) {
+        holder.bindData(adapterData[position])
+    }
+}
---
+++ app/src/main/java/com/misa/fresher/Base/BaseViewHolder.kt
@@ -0,0 +1,18 @@
+package com.misa.fresher.Base
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder<T>(view: View) : RecyclerView.ViewHolder(view) {
+
+    abstract var clickItemListener: (T) -> Unit
+
+    open fun bindData(item: T) {
+        itemView.setOnClickListener { clickItemListener(item) }
+    }
+
+
+
+
+
+}
