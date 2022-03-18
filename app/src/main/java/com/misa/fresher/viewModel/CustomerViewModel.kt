6aefa6ca2ce0d571790995640f50cd36cdf7c853
NGUYEN THAI BAO <baopzzoo@gmail.com>
2022-03-18 13:27:48 +0700

    Add CustomerViewModel.kt

---
+++ app/src/main/java/com/misa/fresher/viewModel/CustomerViewModel.kt
@@ -0,0 +1,16 @@
+package com.misa.fresher.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.misa.fresher.model.Customer
+
+
+class CustomerViewModel: ViewModel() {
+    private var _customer = MutableLiveData<Customer>()
+    val customer : LiveData<Customer> get() = _customer
+    fun addCustomer(cus:Customer){
+        _customer.value=cus
+        _customer.postValue(_customer.value)
+    }
+}
