7c29b7f61fc25129a7bc47a9c9b755adc8a8c0da
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2022-03-18 21:36:15 +0700

    New Commit

--- app/src/main/java/com/misa/fresher/MainActivity.kt
+++ app/src/main/java/com/misa/fresher/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.misa.fresher
 
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -13,13 +13,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-import com.misa.fresher.Fragment.Bill.ListBillFragment
-import com.misa.fresher.Fragment.Sale.SaleFragment
 import com.misa.fresher.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.fragment_sale.view.*
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     private var appBarConfiguration: AppBarConfiguration? = null
     lateinit var binding: ActivityMainBinding
     private lateinit var shareViewModel: PublicViewModel
@@ -33,23 +30,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(binding.root)
-        val navHost =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHost.navController
+
         shareViewModel = ViewModelProvider(this).get(PublicViewModel::class.java)
 
-        val toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.root.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        binding.drawerLayout.addDrawerListener(toggle)
 
-        binding.navSaleFragment.setNavigationItemSelectedListener(this)
-
-        displayScreen(-1)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration =
+            AppBarConfiguration(navController.graph, findViewById<DrawerLayout>(R.id.drawer_layout))
+        appBarConfiguration.let {
+            (findViewById<NavigationView>(R.id.navSaleFragment)).setupWithNavController(
+                navController
+            )
+        }
 
 //        configureXML()
     }
@@ -67,19 +61,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             toggleDrawer(binding.navSaleFragment)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_nav, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_Sale -> return true
-            R.id.nav_ListBill -> return true
-            else -> return super.onOptionsItemSelected(item)
         }
     }
 
@@ -103,27 +84,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return appBarConfiguration?.let {
             navController.navigateUp(it)
         } == true || super.onSupportNavigateUp()
-    }
-
-    fun displayScreen(id: Int) {
-        when (id) {
-            R.id.nav_Sale -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.navHostFragment, SaleFragment()).commit()
-            }
-
-            R.id.nav_ListBill -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.navHostFragment, ListBillFragment()).commit()
-            }
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        displayScreen(item.itemId)
-
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 
 }
