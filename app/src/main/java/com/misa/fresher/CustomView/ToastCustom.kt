6596cb436a570c135ae35ba07d434725ba663614
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2022-03-18 20:55:04 +0700

    Toast custom

--- app/src/main/java/com/misa/fresher/CustomView/ToastCustom.kt
+++ app/src/main/java/com/misa/fresher/CustomView/ToastCustom.kt
@@ -7,8 +7,8 @@ import android.widget.TextView
 import android.widget.Toast
 import com.misa.fresher.R
 
-class ToastCustom(context: Context): Toast(context) {
-    fun makeToast(context: Context, message: String, duration: Int): Toast{
+object ToastCustom {
+    fun makeToast(context: Context, message: String, duration: Int): Toast {
         var toast = Toast(context)
         var layout = LayoutInflater.from(context).inflate(R.layout.toast_custom, null, false)
         var tvToast = layout.findViewById<TextView>(R.id.tv_toast_message)
