6aa9924710fbb7f8484d48c980b778e07beb9f78
NGUYEN THAI BAO <baopzzoo@gmail.com>
2022-03-18 10:57:43 +0700

    Thêm comment Bills

--- app/src/main/java/com/misa/fresher/model/Bill.kt
+++ app/src/main/java/com/misa/fresher/model/Bill.kt
@@ -1,8 +1,12 @@
 package com.misa.fresher.model
 
-
+/**
+* Tạo class dùng để lưu bill
+* @Auther : NTBao
+* @date : 3/18/2022
+**/
 data class Bill(
     var listSelectedProduct: MutableList<SelectedProducts>?,
     val id: Int,
-    var receiver: Receiver?
+    var customer: Customer?
 )
