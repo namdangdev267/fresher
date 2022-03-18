85213787ec236a685ea1aec51c340af48a1ce0eb
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2022-03-18 08:40:25 +0700

    Main activity

--- app/src/main/java/com/misa/fresher/MainActivity.kt
+++ app/src/main/java/com/misa/fresher/MainActivity.kt
@@ -2,25 +2,34 @@ package com.misa.fresher
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
-import com.misa.fresher.Models.Product
 import com.misa.fresher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var appBarConfiguration: AppBarConfiguration? = null
     lateinit var binding: ActivityMainBinding
+    lateinit var shareViewModel: PublicViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(binding.root)
+
+        shareViewModel = ViewModelProvider(this).get(PublicViewModel::class.java)
 
         configureXML()
     }
