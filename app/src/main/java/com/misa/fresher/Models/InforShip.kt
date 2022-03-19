3dbdcf575adeec173741f11b634ff403f24295d9
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2022-03-19 13:02:54 +0700

    Model

--- app/src/main/java/com/misa/fresher/Models/InforShip.kt
+++ app/src/main/java/com/misa/fresher/Models/InforShip.kt
@@ -3,15 +3,15 @@ package com.misa.fresher.Models
 import com.misa.fresher.Models.Enum.DepositMethod
 import com.misa.fresher.Models.Enum.SaleChannel
 
-data class InforShip (
-    var receiver: String,
-    var tel:String,
-    var address:String,
-    var area:String?,
-    var ward:String?,
-    var shipPaid:Float,
-    var depositMethod: DepositMethod,
-    var deposit:Float,
-    var saleChannel:SaleChannel,
-    var collectCOD:Boolean
+data class InforShip(
+    var receiver: String?,
+    var tel: String?,
+    var address: String?,
+    var area: String?,
+    var ward: String?,
+    var shipPaid: Float?,
+    var depositMethod: DepositMethod?,
+    var deposit: Float?,
+    var saleChannel: SaleChannel?,
+    var collectCOD: Boolean
 )
